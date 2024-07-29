76715a7c0477c609b2aa47ad7004b9b8599b54e7
Heera Singh Lodhi <heera9331@gmail.com>
2024-07-30 04:48:49 +0530

    port change

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 import io from "socket.io-client";
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:4000");
 
 export async function POST(req, res) {
   try {
